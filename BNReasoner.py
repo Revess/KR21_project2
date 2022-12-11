5b85461fbcf0a20a398a6fc507ec67d420c754da
florencecornelissen <f.y.cornelissen@student.vu.nl>
2022-12-11 17:27:49 +0100

    Changes Flo

--- BNReasoner.py
+++ BNReasoner.py
@@ -51,8 +51,11 @@ class BNReasoner:
         res = pd.DataFrame(columns=df.columns.drop([variable]))
 
         sort = list(df.columns)
+        print('columns',sort)
         sort.remove('p')
+        print('column wo p', sort)
         sort.remove(variable)
+        print('after remove var', sort)
 
         len_ins = len([col for col in df.columns if 'ins. of' in col])
         if len_ins == 1:
@@ -60,9 +63,11 @@ class BNReasoner:
 
         df = df.sort_values(by = sort, ignore_index=True)
 
+        print('df',df)
         for i in range(len(df.iloc[:,0])):
             if i % 2 == 0:
                 max = df.loc[i:i, ['p', variable]]
+                print('this is the max',max)
             else:
                 if df.loc[i, 'p'] > max.iloc[0, 0]:
                     max = df.loc[i:i, ['p', variable]]
@@ -140,78 +145,101 @@ class BNReasoner:
         else:
             print('wrong heuristic chosen, pick either min-degree or min-fill')
 
-    def variableElimination(self, query, evidence=dict()):
+    def variableElimination(self, query=dict(), evidence=dict(), elements=list()):
         print(evidence)
         cpts = self.bn.get_all_cpts()
         instantiation = pd.Series(evidence)
         self.bn.draw_structure()
         print(cpts)
 
-        order = reasoner.Ordering('min-degree')
+        order = self.Ordering('min-degree')
+        for x in elements:
+            order.remove(x)
 
         for i in order:
             if i not in query:
                 if i not in evidence.keys():
-                    result = self.marginalization(1, reasoner.bn.get_all_cpts()[i])
+                    result = self.marginalization(1, self.bn.get_all_cpts()[i])
                     self.bn.update_cpt(i,result)
-        print()
+                    
 
-    def marginalDistributions(self, query, evidence=dict()):
+    def marginalDistributions(self, query=list(), evidence=list()):
         cpts = self.bn.get_all_cpts()
+        instantiation = pd.Series(evidence)
 
+        for i in cpts:
+            reducing = self.bn.reduce_factor(instantiation, self.bn.get_all_cpts()[i])
+            self.bn.update_cpt(i, reducing)
 
-        reducing = self.reduceNet(evidence)
-        
         graph = self.bn.get_interaction_graph()
-        nx.all_simple_paths(graph,source=X[0],target=Y[0])
+        nx.all_simple_paths(graph,source=X[0],target=Y[0]))
 
         Qande = []
 
         for node in cpts.keys():
             for q in query:
-                if q in cpts.iloc[0]:
+                if q in self.bn.get_all_cpts()[node].iloc[0]:
                     Qande.append(node)
 
         for node in cpts.keys():
             for e in evidence.keys():
-                if e in cpts.iloc[0]:
+                if e in self.bn.get_all_cpts()[node].iloc[0]:
                     Qande.append(node)
         
-        print(Qande)
+        Qande = list(dict.fromkeys(Qande))
 
-        neededcpts = {}
+        doVarEl = self.variableElimination(query, evidence, Qande)
 
-        for item in Qande:
-            neededcpts[item] = self.bn.get_cpt(item)
+        for q in query:
+            result = self.marginalization(1, self.bn.get_all_cpts()[q])
+
+
+        #neededcpts = {}
+
+        #for item in Qande:
+        #   neededcpts[item] = self.bn.get_cpt(item)
 
         #have to use order with the only the cpts I got, can I give them to this function?
 
-        for q in query:
-            result = self.marginalization(q, )
+        #for q in query:
+        #    result = self.marginalization(q, )
 
-    def mapping(self, query, evidence=dict()):
+        '''for q in query:
+            for e in evidence.keys():
+                routes = list(nx.all_simple_paths(graph,source=query,target=evidence))
+                print(routes)'''
+        
+        '''routes = self.bn.get_interaction_graph().adj
+
+        nodes = []
+        for item in routes.keys():
+            if item in query:
+                nodes.append(routes.values())
+
+        print(nodes)'''
+
+
+    def mapping(self, query=dict(), evidence=dict()):
         
         order = self.Ordering('min-degree')
-        
-        print('begin order', order)
 
         order = [x for x in order if x not in query]
-
-        print('later order',order)
+        print(order)
 
         for i in order:
-            result = self.marginalization(i, reasoner.bn.get_all_cpts()[i])
+            result = self.marginalization(i, self.bn.get_all_cpts()[i])
+            print('with marg:',result)
             self.bn.update_cpt(i, result)
 
         for q in query:
-            domax = self.maxingOut(q)
+            domax = self.maxingOut(q, self.bn.get_all_cpts()[q])
             self.bn.update_cpt(q, domax)
 
-            print(domax.loc[:,'p'])
-            print(domax.loc[:,'ins. of ' + q])
+        print(domax.loc[:,'p'])
+        print(domax.loc[:,'ins. of ' + q])
 
-            print(domax['p'])
-            print(domax.loc[:,'ins. of' + q])
+            #print(domax['p'])
+            #print(domax.loc[:,'ins. of' + q])
 
 
         #compute P(Q,e) first with variable elimination, then maximize-out Q using extended variables
@@ -223,7 +251,7 @@ class BNReasoner:
         for i in order:
             if i not in query:
                 if i not in evidence.keys():
-                    result = self.maxingOut(i)
+                    result = self.maxingOut(i, self.bn.get_all_cpts()[i])
                     self.bn.update_cpt(i, result)
                     print('this is the cpt:', result)
                     ending.append(result)
@@ -232,8 +260,8 @@ class BNReasoner:
 
         for q in query:
 
-            print(result['p']) #this should work, right? print the p value of the query cpts
-            print(result['ins. of ' + q])
+            print(result.loc[:,'p'])
+            print(result.loc[:,'ins. of ' + q])
         #return query['p']
         print('this is the ending cpt:', ending)
 
@@ -255,3 +283,9 @@ class BNReasoner:
 reasoner = BNReasoner("./testing/dog_problem.BIFXML")
 x = reasoner.maxingOut(variable='dog-out',cpt = reasoner.bn.get_all_cpts()['dog-out'])
 
+
+#print(reasoner.mapping(query = {'dog-out'}, evidence = {'hear-bark': True}))
+#print(reasoner.marginalization('dog-out', reasoner.bn.get_all_cpts()['dog-out']))
+#print(reasoner.maxingOut('dog-out', reasoner.bn.get_all_cpts()['dog-out']))
+#print(reasoner.mpe(query = {'dog-out'}, evidence = {'hear-bark': True}))
+print(reasoner.marginalDistributions(query = {'dog-out'}, evidence = {'dog-out': True}))
